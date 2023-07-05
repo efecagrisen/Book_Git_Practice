0a75a89118bccc12559b8526da7f46a3439718f9
efecagrisen <efecagrisen@yahoo.com>
2023-07-05 16:05:19 +0300

    feature-20 created

---
+++ src/BranchingPractice2.java
@@ -0,0 +1,9 @@
+package src;
+
+public class BranchingPractice2 {
+    public static void main(String[] args) {
+        System.out.println("Task1");
+        System.out.println("Create a new branch feature-20");
+
+    }
+}
